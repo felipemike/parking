1d588f580228ef16728c5d4d94c377eaa9506f21
felipemike <felipeclemente2012@gmail.com>
2023-06-01 22:52:07 -0300

    Criação da logica para adicionar e remover.

---
+++ script.ts
@@ -0,0 +1,49 @@
+interface Veiculo {
+  nome: string;
+  placa: string;
+  entrada: Date;
+  saida?: Date;
+}
+
+
+(function () {
+const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+
+function patio() {
+
+  function ler(){}
+
+  function adicionar(veiculo: Veiculo){
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>${veiculo.nome}</td>
+    <td>${veiculo.placa}</td>
+    <td>${veiculo.entrada}</td>
+    <td> 
+      <button class="delete" data-placa="${veiculo.placa}">X</button>
+    </td>
+    `;
+    $("#patio")?.appendChild(row);
+  }
+
+  function remover(){}
+
+  function salvar(){}
+
+  function listar(){}
+
+  return { ler, adicionar, remover, salvar, listar };
+}
+
+$("#cadastrar")?.addEventListener("click", () => {
+  const nome = $("#nome").value;
+  const placa = $("#placa").value;
+  if (!nome || !placa) {
+    alert("Preencha todos os campos!");
+    return;
+  }
+
+  patio().adicionar({ nome, placa, entrada: new Date() });
+
+});
+})();
